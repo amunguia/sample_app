a64b9bba915aa95d54b81b6e7d3d9378622f02c9
Alex <munguia.jandro@gmail.com>
2014-09-04 20:20:45 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,13 +5,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+  
+    it "should have the title base title" do
+      visit root_path
+      expect(page).not_to have_title("| Home")
     end
   
   end
@@ -19,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
     
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   
@@ -33,15 +38,28 @@ describe "Static pages" do
   describe "About page" do
     
     it "should have the content 'About Us'" do 
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
   
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end 
   
+  end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 
 end
